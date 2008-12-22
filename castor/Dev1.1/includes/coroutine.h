590a83861f964e94ac3acdbd64fd0ca6e1cbea8e
Roshan <Roshan@a2de7dd8-3f72-5846-8ac6-a2c5def5075f>
2008-12-22 21:32:41 +0000

    Bug fix in Coroutine.h to avoid double evaluation of predicate argument in co_yield

--- castor/Dev1.1/includes/coroutine.h
+++ castor/Dev1.1/includes/coroutine.h
@@ -1,3 +1,7 @@
+// Castor : Logic Programming Library
+// Copyright © 2008 Roshan Naik (roshan@mpprogramming.com).
+// This software is goverened by the MIT license (http://www.opensource.org/licenses/mit-license.php).
+
 #if !defined CASTOR_COROUTINE_H
 #define CASTOR_COROUTINE_H 1
 
@@ -14,7 +18,7 @@ public:
 
 // macros for implementing relations imperatively (as coroutines)
 #define co_begin()    switch (co_entryPt) { case 0:
-#define co_yield(x)   { co_entryPt = (x)? __LINE__ : -1; return (x); case __LINE__: ; }
+#define co_yield(x)   { co_entryPt = (x)? __LINE__ : -1; return (co_entryPt==__LINE__); case __LINE__: ; }
 #define co_return(x)  { co_entryPt = -1; return (x); }
 #define co_end()      break; } co_entryPt = -1; return false;
 
