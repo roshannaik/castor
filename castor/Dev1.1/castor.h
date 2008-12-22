4c4c29d3e353540900bb3ab9fc9befd80a0391c4
Roshan <Roshan@a2de7dd8-3f72-5846-8ac6-a2c5def5075f>
2008-12-22 22:08:29 +0000

    castor.h: including named_iles.h

--- castor/Dev1.1/castor.h
+++ castor/Dev1.1/castor.h
@@ -6,16 +6,17 @@
 #if !defined CASTOR_H
 #define CASTOR_H 1
 
+#include "includes/coroutine.h"
 #include "includes/lref.h"
 #include "includes/relation.h"
 #include "includes/recurse.h"
 #include "includes/predicate.h"
-#include "includes/functional.h"
 #include "includes/eq.h"
 #include "includes/ops.h"
 #include "includes/utils.h"
 #include "includes/cut.h"
 #include "includes/ile.h"
+#include "includes/named_iles.h"
 #include "includes/io.h"
 
 
