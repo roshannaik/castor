6cab6df42d09e588b0412406bc9cc3bf95056b7f
tzlaine <tzlaine@a2de7dd8-3f72-5846-8ac6-a2c5def5075f>
2010-05-26 20:44:08 +0000

    Added the first two Boost unit tests on the Boost branch, test_permute and test_shuffle.  These were adapted from test_collection, which is now removed.

--- castor/branches/boost/libs/castor/test/test_collection.h
+++
@@ -1,8 +0,0 @@
-#if !defined TEST_CASTOR_COLLECTION_H
-#define TEST_CASTOR_COLLECTION_H 1
-
-void test_shuffle();
-void test_permute();
-
-//test_accumulate
-#endif
---
+++ castor/branches/boost/libs/castor/test/test_permute.cpp
@@ -0,0 +1,33 @@
+#include <boost/castor.h>
+
+#include <boost/test/minimal.hpp>
+
+
+using namespace castor;
+
+int test_main(int, char * [])
+{
+    { // test mode
+        lref<std::string> s = "hello", ps = "olleh";
+        relation r = permute(s, ps);
+
+        BOOST_CHECK(r());
+        BOOST_CHECK(!r());
+    }
+
+    { // generate mode
+        lref<int> c = 0;
+        lref<std::string> s = "hello", ps, ss = "helol";
+        relation r = permute(s, ps) && eq(ps, ss) && eval(++c);
+
+        BOOST_CHECK(r() || *c == 1);
+
+        ss = "heoll";
+
+        BOOST_CHECK(r() || *c == 2);
+
+        BOOST_CHECK(!r());
+    }
+
+    return 0;
+}
