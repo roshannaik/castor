0e44ecade83f08aeaf501d44db7d04b7c11ec599
tzlaine <tzlaine@a2de7dd8-3f72-5846-8ac6-a2c5def5075f>
2010-05-26 19:43:11 +0000

    Rearranged the files and directories in the Boost branch to match the Boost directory structure.  All operations in this commit are moves, except for the deletion of package.bat, which is not useful in a Boost-built context.

--- castor/branches/boost/package.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Documents and Settings\Roshan\My Documents\Visual Studio 2005\Projects
-del castor\castor.zip
-winrar a castor\castor castor\README.txt castor\LICENSE.txt castor\castor.h castor\includes\*.h castor\docs\*.pdf castor\test\*.cpp 
-cd -
